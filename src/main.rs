486b1a0f42a6b952f76284ad74d9ef64f77be4c6
Ryan Vacek <ryanvacek@Ryans-MacBook-Pro.local>
2023-05-10 13:10:17 -0600

    some changes

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ trait MarkovState {
 
 pub struct State {
     state: [u8; 2],
-    producer: Option<Sender<[u8; 2]>>,
+    producer: Option<Sender<[f32; 2]>>,
 }
 
 impl State {
@@ -23,12 +23,12 @@ impl State {
             producer: None,
         }
     }
-    fn add_producer(&mut self, producer: Sender<[u8; 2]>) {
+    fn add_producer(&mut self, producer: Sender<[f32; 2]>) {
         self.producer = Some(producer)
     }
 }
 
-
+/*
 impl Stream for State {
     type Item = &'static str;
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<&'static str>> {
@@ -38,19 +38,20 @@ impl Stream for State {
         Poll::Pending
     }
 }
+*/
 
 pub struct MarkovMachine<'a> {
     futures: Option<Vec<&'a mut State>>,
     transition_matrix: [[f32; 3]; 3],
     buffer: [u8; 9],
     buffer_position: usize,
-    receiver: Receiver<u8>,
+    receiver: Receiver<[f32; 2]>,
 }
 
 impl<'a> MarkovMachine<'a> {
-    fn new() -> (Self, Sender<u8>) {
+    fn new() -> (Self, Sender<[f32; 2]>) {
         let transition_matrix: [[f32; 3]; 3] = [[0.2, 0.3, 0.5], [0.6, 0.2, 0.2], [0.1, 0.4, 0.5]];
-        let (sender, receiver) = channel(100);
+        let (sender, receiver) = channel(1);
         let markov_machine = MarkovMachine {
             transition_matrix: transition_matrix,
             futures: None,
@@ -64,13 +65,14 @@ impl<'a> MarkovMachine<'a> {
     fn states(&self) -> usize {
         let length_width = self.transition_matrix[0].len();
         let length_height = self.transition_matrix.len();
-        return length_width*length_height
+        return length_width * length_height;
     }
     fn add_states(&self, states: Vec<&'a mut State>) {
         self.futures = Some(states)
     }
 }
 
+/*
 impl Sink<u8> for MarkovMachine<'_> {
     type Error = Error;
 
@@ -109,6 +111,12 @@ impl Sink<u8> for MarkovMachine<'_> {
         Poll::Ready(Ok(()))
     }
 }
+*/
 
 fn main() {
-    let markov_machine = MarkovMachine::new();
+    let (markov_machine, producer) = MarkovMachine::new();
+    let states: Vec<State> = Vec::new();
+    for i in 0..markov_machine.states() {
+        let state = State::new([0, 0]).add_producer(producer.clone());
+    }
+}
